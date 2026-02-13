dd4b1e29e68b00a266e866a4808be7e0ef4e1aa8
madhuerpdirect-droid <madhu.erpdirect@gmail.com>
2026-02-13 15:18:19 +0530

    Change loadEnv prefix to 'VITE_'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   // This line must be exactly like this to pull the key from GitHub
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
 
   return {
     plugins: [react()],
